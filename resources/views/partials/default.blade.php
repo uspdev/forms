18960b59ce25071abff2246d27f36c5f2f8075fe
Masaki Kawabata Neto <kawabata@usp.br>
2025-05-29 17:00:14 -0300

    incluindo type perdido

--- resources/views/partials/default.blade.php
+++ resources/views/partials/default.blade.php
@@ -2,7 +2,7 @@
   
   <label for="{{ $field['id'] }}">{{ $field['label'] }} {!! $field['requiredLabel'] !!}</label>
 
-  <input id="{{ $field['id'] }}" name="{{ $field['name'] }}" class="{{ $field['controlClass'] }}"
+  <input id="{{ $field['id'] }}" type="{{ $field['type'] }}" name="{{ $field['name'] }}" class="{{ $field['controlClass'] }}"
     value="{{  $field['value'] ?? $field['old'] }}" @required($field['required'])>
     
 </div>
