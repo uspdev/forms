3a47ef71bfa6dd78d72ae29091887a80c4fd8f25
Lucas Lopes <lucaslops12@gmail.com>
2026-03-19 17:05:54 -0300

    feat: Atualiza a lógica de exibição para longName nas views local-usp e patrimonio-usp.

--- resources/views/partials/patrimonio-usp-view.blade.php
+++ resources/views/partials/patrimonio-usp-view.blade.php
@@ -1,11 +1,20 @@
 @php
   $numpat = $submission['data'][$field['name']] ?? 'n/a';
+  $codigoFormatado = str_pad(substr($numpat, 0, -6), 3, '0', STR_PAD_LEFT) . '.' . str_pad(substr($numpat, -6), 6, '0', STR_PAD_LEFT);
   $title = '';
   if ($bemPatrimoniado = \Uspdev\Replicado\Bempatrimoniado::dump($numpat)) {
       $title = $bemPatrimoniado['epfmarpat'] . ' - ' . $bemPatrimoniado['tippat'] . ' - ' . $bemPatrimoniado['modpat'];
   }
+
+  if ($longName ?? false) {
+      // Modo longo: código + descrição completa do patrimônio
+      $display = $codigoFormatado . (!empty($title) ? ' - ' . $title : '');
+  } else {
+      // Modo curto: apenas o código (comportamento padrão)
+      $display = $codigoFormatado;
+  }
 @endphp
 
 <span title="{{ $title }}">
-  {{ str_pad(substr($numpat, 0, -6), 3, '0', STR_PAD_LEFT) . '.' . str_pad(substr($numpat, -6), 6, '0', STR_PAD_LEFT) }}
+  {{ $display }}
 </span>
