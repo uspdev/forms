1501063319ff2732848cb33df0ce01015c24f95f
Lucas Lopes <lucaslops12@gmail.com>
2026-03-19 14:49:57 -0300

    Cria botão de visualização do modal

---
+++ resources/views/submission/partials/show-btn.blade.php
@@ -0,0 +1,5 @@
+<div>
+    <button type="button" class="btn btn-sm btn-outline-primary" data-toggle="modal" data-target="#showFormSubmissionModal-{{ $submission->id }}">
+        <i class="fa fa-eye"></i>
+    </button>
+</div>
