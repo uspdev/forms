cd1fc7782ee9b0ea270a4bb421914ec882b38324
Lucas Lopes <lucaslops12@gmail.com>
2026-03-20 16:29:05 -0300

    fix: ajusta botoes para respeitarem o seu tamanho

--- resources/views/definition/partials/exportdefinition-btn.blade.php
+++ resources/views/definition/partials/exportdefinition-btn.blade.php
@@ -1,3 +1,5 @@
-<a href="{{ route('form-definitions.export-definition',  $formDefinition) }}" class="btn btn-sm btn-info ml-2">
-  Exportar
-</a>
+<div>
+  <a href="{{ route('form-definitions.export-definition',  $formDefinition) }}" class="btn btn-sm btn-info ml-2">
+    Exportar
+  </a>
+</div>
--- resources/views/definition/partials/show-btn.blade.php
+++ resources/views/definition/partials/show-btn.blade.php
@@ -1,3 +1,5 @@
-<a href="{{ route('form-submissions.index', $formDefinition) }}" class="btn btn-sm btn-primary">
-  <i class="fas fa-eye"></i> Ver submissões
-</a>
+<div>
+  <a href="{{ route('form-submissions.index', $formDefinition) }}" class="btn btn-sm btn-primary">
+    <i class="fas fa-eye"></i> Ver submissões
+  </a>
+</div>
